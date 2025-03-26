54dba0284cda4db607c2dff91036aef4e3554e1a
Ilannkumaran Ilangovan <kumaranilan@yahoo.com>
2025-03-26 08:50:17 +0000

    Launching job in parallel

--- src/main/java/com/ilan/config/DefaultBatchConfig.java
+++ src/main/java/com/ilan/config/DefaultBatchConfig.java
@@ -24,6 +24,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 @EnableBatchProcessing(dataSourceRef = "dataSource",
         transactionManagerRef = "transactionManager",
         tablePrefix = "SPRING_BATCH.BATCH_",
+        maxVarCharLength = 1000, // https://docs.spring.io/spring-batch/reference/job/configuring-repository.html
         isolationLevelForCreate = "ISOLATION_READ_COMMITTED",
         taskExecutorRef = "customJobTaskExecutor")
 @Configuration
