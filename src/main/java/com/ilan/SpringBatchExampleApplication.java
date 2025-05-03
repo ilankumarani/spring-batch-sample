02adc733f7e83ca6f757cab00400a8785cc03fc6
Ilannkumaran Ilangovan <kumaranilan@yahoo.com>
2025-05-03 18:22:19 +0100

    Refactoring the design

--- src/main/java/com/ilan/SpringBatchExampleApplication.java
+++ src/main/java/com/ilan/SpringBatchExampleApplication.java
@@ -23,39 +23,29 @@ import static com.ilan.constants.JobConstants.UU_ID;
 @SpringBootApplication
 public class SpringBatchExampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBatchExampleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchExampleApplication.class, args);
+    }
 
-	@Bean
-	public TaskExecutor jobTaskExecutor() {
-		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-		executor.setCorePoolSize(5);  // Number of concurrent threads
-		executor.setMaxPoolSize(10);
-		executor.setQueueCapacity(25);
-		executor.initialize();
-		return executor;
-	}
+    @Bean
+    public TaskExecutor jobTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);  // Number of concurrent threads
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(25);
+        executor.initialize();
+        return executor;
+    }
 
-	@Bean
-	CommandLineRunner startJob(
-			//@Qualifier("customJobLauncher")
-			 JobLauncher jobLauncher, Job asyncJob) {
-		return args -> {
-            for (int i = 0; i < 1; i++) {
-//                jobTaskExecutor().execute(() -> {
-//                    try {
-			UUID uuid = UUID.randomUUID();
-			JobParameters jobParameters = new JobParametersBuilder()
-					.addString(UU_ID, uuid.toString())
-					.addString(FILE_NAME_PARAM, FILE_NAME)
-					.toJobParameters();
-			jobLauncher.run(asyncJob, jobParameters);
-//                    } catch (Exception e) {
-//                        e.printStackTrace();
-//                    }
-//                });
-            }
-		};
-	}
+    @Bean
+    CommandLineRunner startJob(JobLauncher jobLauncher, Job asyncJob) {
+        return args -> {
+            UUID uuid = UUID.randomUUID();
+            JobParameters jobParameters = new JobParametersBuilder()
+                    .addString(UU_ID, uuid.toString())
+                    .addString(FILE_NAME_PARAM, FILE_NAME)
+                    .toJobParameters();
+            jobLauncher.run(asyncJob, jobParameters);
+        };
+    }
 }
